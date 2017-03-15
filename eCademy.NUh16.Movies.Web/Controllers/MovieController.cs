141ab06338317552e1358847a743c3aab1054749
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2017-03-15 20:24:05 +0100

    2 - Controllers i MVC

---
+++ eCademy.NUh16.Movies.Web/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using eCademy.NUh16.Movies.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCademy.NUh16.Movies.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private static List<string> db = new List<string> {
+            "James Bond - Dr. No",
+            "Iron Man"
+        };
+
+        // GET: Movie
+        public ViewResult Index(string search)
+        {
+            var movies = (search == null)
+                ? db.ToArray()
+                : db.Where(name => name.ToLower().Contains(search.Trim().ToLower()))
+                    .ToArray();
+
+            var viewModel = new MovieSearchViewModel
+            {
+                Search = search,
+                Movies = movies
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string movie)
+        {
+            db.Add(movie);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ eCademy.NUh16.Movies.Web/Models/MovieSearchViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace eCademy.NUh16.Movies.Web.Models
+{
+    public class MovieSearchViewModel
+    {
+        public string Search { get; set; }
+        public string[] Movies { get; set; }
+    }
+}
